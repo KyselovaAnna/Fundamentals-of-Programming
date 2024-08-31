ebde499cf17ebcb456a6b7be2a9cc1e33992d68e
KyselovaAnna <a.g.kyselova@gmail.com>
2024-08-31 23:47:25 +0300

    Create placeholders.cpp

---
+++ placeholders.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Format Specifications - Practice
+
+int main()
+{
+printf("We have %d coins in the bank\n", 100);
+printf("We have %f coins in the bank\n", 125.7);
+printf("Year = %d\n", 2024);
+printf("Your average grade is: %f \n", 95.13);
+printf("num1 = %d, num2 = %d, sum = %d \n", 5, 7, 5 + 7);
+printf("num1 = %d, num2 = %d, sub = %d \n", 5, 3, 5 - 3);
+printf("a = %d, b = %d, sum = a + b = %d \n", 3, 5, 3 + 5);
+printf("a = %d, b = %d, sum = %d + %d = %d \n", 3, 5, 3, 5, 3 + 5);
+printf("a = %d, b = %d, sum = a + b = %d + %d = %d \n", 3, 5, 3, 5, 3 + 5);
+return 0;
+}
