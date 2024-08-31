662647c4ef1149def80a84dadb525862cebd0ac5
KyselovaAnna <a.g.kyselova@gmail.com>
2024-08-31 19:06:05 +0300

    Create 1.hello.cpp

---
+++ 1.hello.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>  // Include the standard input/output library for using printf
+
+int main()  // Main function - entry point of the program
+{
+    // Print the message "Hello World!" to the console
+    // \n at the end moves the cursor to the next line
+    
+printf("Hello World!\n");
+
+    // Return 0 to indicate that the program finished successfully
+    return 0;
+}
