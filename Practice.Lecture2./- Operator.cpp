67dfde2b79689f3bb0507df2887b09eba39f6c79
KyselovaAnna <a.g.kyselova@gmail.com>
2024-09-10 22:22:11 +0300

    Create - Operator.cpp

---
+++ Practice.Lecture2./- Operator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    // create the costPrice variable and assign 19 to it
+    int costPrice = 19;
+
+    // create the sellingPrice variable and assign 27 to it
+    int sellingPrice = 27;
+
+    // compute the profit and print it
+    int profit = sellingPrice - costPrice;
+    printf("Profit: %d\n", profit);
+
+    return 0;
+}
